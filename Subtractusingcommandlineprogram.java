e4e97c5824f107b9d710f8e5e5a53f12c1062d7b
shubhamkrbhatt96 <61389428+shubhamkrbhatt96@users.noreply.github.com>
2023-06-08 01:02:32 +0530

    addprogram

---
+++ Subtractusingcommandlineprogram.java
@@ -0,0 +1,10 @@
+
+public class Subtractusingcommandlineprogram {
+
+	public static void main(String args[]) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int sub = a - b;
+		System.out.println("The subtraction is" + sub);
+	}
+}
---
+++ usernamevalidationcheck.java
@@ -0,0 +1,9 @@
+
+public class usernamevalidationcheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
